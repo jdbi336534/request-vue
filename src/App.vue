fd8db044cfe5564bf71638e6d0c20e7d899d30a0
jiangdb <jiangdb@r7data.com>
2018-03-26 18:08:55 +0800

    three.js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'App'
-  }
+};
 </script>
 
 <style>
-  #app {
+#app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
-  }
+    height: 100%;
+    width: 100%;
+}
 </style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,24 +1,8 @@
 <!-- 测试代码 -->
 <template>
-    <div>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-            <li>11</li>
-            <li>12</li>
-        </ul>
-    </div>
+  <div id="main"></div>
 </template>
 <script>
-import { login, queryActivities } from '@/services/test';
 export default {
     components: {},
     name: 'Test',
@@ -27,21 +11,45 @@ export default {
     },
     computed: {},
     created() {},
-    mounted() {
-        this.getData();
-    },
+    mounted() {},
     methods: {
-        async loginSystem(userName, password) {
-            let res = await login({ userName, password });
-            console.log(res);
-        },
-        async getData() {
-            let res = await queryActivities();
-            console.log('getData:', res);
+        init() {
+            // 初始化场景
+            let scene = new THREE.Scene();
+            // 初始化相机
+            let FOV = 60;
+            let FAR = 10000;
+            let NEAR = 0.1;
+            let CLIENT_WIDTH = this.$el.clientWidth;
+            let CLIENT_HEIGHT = this.$el.clientHeight;
+            let ASPECT = SCENE_WIDTH / SCENE_HEIGHT;
+            let cameraX = 0;
+            let cameraY = 3;
+            let cameraZ = 6.7;
+            let camera = new THREE.PerspectiveCamera(
+                this.FOV,
+                this.ASPECT,
+                this.NEAR,
+                this.FAR
+            );
+            camera.position.set(
+                this.cameraX *
+                    1.3 *
+                    Math.sin(THREE.Math.degToRad(this.speed + 0.01)),
+                this.cameraY,
+                this.cameraZ *
+                    1.3 *
+                    Math.cos(THREE.Math.degToRad(this.speed + 0.01))
+            );
+            camera.lookAt(scene.position);
+            scene.add(camera);
         }
     }
 };
 </script>
 <style scoped>
-
+#main {
+    height: 100%;
+    width: 100%;
+}
 </style>
